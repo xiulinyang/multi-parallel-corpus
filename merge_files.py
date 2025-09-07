6786b072102dd41b9f7dc3bdafd6f80a062b73be
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-06 21:17:11 -0400

    update merged files

--- merge_files.py
+++ merge_files.py
@@ -1,6 +1,6 @@
 from glob import glob
 from pathlib import Path
-parallel_dirs = glob('*_parallel/')
+parallel_dirs = sorted(glob('*_parallel/'))
 
 langs = ['ar','de', 'fr', 'zh', 'pl', 'ru', 'tr']
 Path("merged").mkdir(exist_ok=True)
@@ -11,6 +11,7 @@ for lang in langs:
         merged_lang = [Path(f'{x}/{lang}.txt').read_text().strip() for x in parallel_dirs]
         merged_en_check = [len(x.split('\n')) for x in merged_en]
         merged_lang_check = [len(x.split('\n')) for x in merged_lang]
+        print(merged_en_check, merged_lang_check)
         assert merged_en_check == merged_lang_check
         en.write('\n'.join(merged_en))
         la.write('\n'.join(merged_lang))
