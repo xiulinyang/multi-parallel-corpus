e6d39ed9d475f2f16b8584c74e0ca967d463b966
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-08 00:36:41 -0400

    fix bug

--- add_new_lang.py
+++ add_new_lang.py
@@ -36,7 +36,7 @@ def find_overlap(corpora, lang, en_p):
     :param file_paths: the paths where the English-centered parallel corpora are located.
     :return: the overlap English sentences across all English-centered parallel corpora.
     '''
-    initial_lang = read_lines(en_p)
+    initial_lang = set(read_lines(en_p))
     for k, v in corpora.items():
         file_path = f'{v[0]}/{lang}_en.txt'
         lang_en = read_lines(file_path)
@@ -51,27 +51,27 @@ def find_overlap(corpora, lang, en_p):
 def main():
 
 
-    for corpus, tdir in corpora.items():
-        cmd = [
-            "opus_get",
-            "-s", "en",
-            "-t", tdir[1],
-            "-d", corpus,
-            "-p", "moses",
-            "-dl", tdir[0],
-            "-r", 'latest',
-        ]
-
-        subprocess.run(cmd, check=True)
-        subprocess.run("yes | unzip '*.zip'", shell=True, cwd=tdir[0])
-        subprocess.run("rm -f *.zip", shell=True, cwd=tdir[0])
-        src_en = f"{tdir[0]}/{corpus}.en-{tdir[1]}.en"
-        dst_en = f"{tdir[0]}/{LANG}_en.txt"
-
-        src_lang = f"{tdir[0]}/{corpus}.en-{tdir[1]}.{tdir[1]}"
-        dst_lang = f"{tdir[0]}/{LANG}.txt"
-        os.rename(src_en, dst_en)
-        os.rename(src_lang, dst_lang)
+    # for corpus, tdir in corpora.items():
+    #     cmd = [
+    #         "opus_get",
+    #         "-s", "en",
+    #         "-t", tdir[1],
+    #         "-d", corpus,
+    #         "-p", "moses",
+    #         "-dl", tdir[0],
+    #         "-r", 'latest',
+    #     ]
+    #
+    #     subprocess.run(cmd, check=True)
+    #     subprocess.run("yes | unzip '*.zip'", shell=True, cwd=tdir[0])
+    #     subprocess.run("rm -f *.zip", shell=True, cwd=tdir[0])
+    #     src_en = f"{tdir[0]}/{corpus}.en-{tdir[1]}.en"
+    #     dst_en = f"{tdir[0]}/{LANG}_en.txt"
+    #
+    #     src_lang = f"{tdir[0]}/{corpus}.en-{tdir[1]}.{tdir[1]}"
+    #     dst_lang = f"{tdir[0]}/{LANG}.txt"
+    #     os.rename(src_en, dst_en)
+    #     os.rename(src_lang, dst_lang)
 
     find_overlap(corpora, LANG, EN_ALIGN)
 
