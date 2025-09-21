22dbab8963fcbeb78cd9deb6d9b1ffc2da35aa82
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-21 18:14:07 -0400

    update filtering

--- clean_and_compile_data_test.py
+++ clean_and_compile_data_test.py
@@ -12,7 +12,7 @@ import re, unicodedata
 from tqdm import tqdm
 model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
 model = fasttext.load_model(model_path)
-
+import pandas as pd
 
 
 EXPECTED_SCRIPT = {
@@ -72,23 +72,21 @@ def ngram_jaccard(a: str, b: str, n: int = 1):
 def clean_en(
     languages,
     illegal_log_name = 'filtered_out_data.tsv',
-    merged_dir="merged_parallel",
+    merged_dir="en_zh_judgements.tsv",
     len_ratio_min=0.2,
     len_ratio_max=4.0,
     min_tgt_len=3,
     max_punct_digit_ratio=0.5,
     min_script_ratio=0.5,
-    ngram_overlap=3
+    ngram_overlap=2
 ):
     with open(illegal_log_name, "a", encoding="utf-8") as illegal_log:
         kept = []
 
         for lang in tqdm(languages):
-            en_path = Path(merged_dir) / f"{lang}_en.txt"
-            lg_path = Path(merged_dir) / f"{lang}.txt"
-
-            en_text = en_path.read_text(encoding="utf-8").strip().split("\n")
-            lg_text = lg_path.read_text(encoding="utf-8").strip().split("\n")
+            tsv_f = pd.read_csv(merged_dir, sep="\t").to_dict(orient="records")
+            en_text = [x['en'] for x in tsv_f]
+            lg_text = [x['trans'] for x in tsv_f]
             assert len(en_text) == len(lg_text), f"Length mismatch for {lang}"
 
             labels, _ = model.predict(lg_text)
@@ -97,6 +95,13 @@ def clean_en(
             for en_sent, lg_sent, lbl in tqdm(zip(en_text, lg_text, labels)):
                 en_s = en_sent.strip()
                 lg_s = lg_sent.strip()
+                print(ngram_jaccard(en_s, lg_s))
+                if ngram_jaccard(en_s, lg_s) < ngram_overlap:
+                    print('hhh')
+                    print(en_sent, lg_sent)
+                    illegal_log.write(f'{lang}\t{en_sent}\t{lg_sent}\ttoo much overlap.\t_\n')
+                    continue
+
                 if len(lg_s) <= min_tgt_len:
                     illegal_log.write(f'{lang}\t{en_sent}\t{lg_sent}\ttext too short.\t{len(lg_s)}\n')
                     continue
@@ -124,53 +129,52 @@ def clean_en(
                         continue
 
                 # overlap with english
-                if ngram_jaccard(en_s, lg_s) < ngram_overlap:
-                    illegal_log.write(f'{lang}\t{en_sent}\t{lg_sent}\ttoo much overlap.\t_\n')
-                    continue
+
 
                 kept.append(en_s)
-
+    print(len(kept))
     #deduplicate
     return list(dict.fromkeys(kept))
 
-
-def build_en_source_dict(lang, multilingual_parallel_dirs):
-    lang_source_dict = {}
-    for m_dir in multilingual_parallel_dirs:
-        en_data = Path(f'{m_dir}/{lang}_en.txt').read_text().strip().split('\n')
-        m_data = Path(f'{m_dir}/{lang}.txt').read_text().strip().split('\n')
-        assert len(en_data) == len(m_data)
-        source_name = m_dir.split('_')[0]
-        for sent_en, sent_l in zip(en_data, m_data):
-            if sent_en not in lang_source_dict:
-                lang_source_dict[sent_en] = {'source':[source_name],
-                                          'trans': [sent_l]}
-            else:
-                lang_source_dict[sent_en]['source'].append(source_name)
-                lang_source_dict[sent_en]['trans'].append(sent_l)
-    return lang_source_dict
-
-def build_lang_pair(en_text_list, languages, multilingual_parallel_dirs, out_dir='cleaned_parallel'):
-    out_dir = Path(out_dir)
-    out_dir.mkdir(parents=True, exist_ok=True)
-    for lang in tqdm(languages):
-        meta = build_en_source_dict(lang, multilingual_parallel_dirs)
-        out_path = Path(f'{out_dir}/en_{lang}.tsv')
-        with out_path.open("w", encoding="utf-8", newline="") as f:
-            writer = csv.writer(f, delimiter="\t")
-            writer.writerow(["en", "trans", "source"])
-
-            for en_sent in en_text_list:
-                if en_sent not in meta:
-                    continue
-                trans = meta[en_sent]["trans"][0]
-                source = "&".join(meta[en_sent]["source"])
-                writer.writerow([en_sent, trans, source])
+#
+# def build_en_source_dict(lang, multilingual_parallel_dirs):
+#     lang_source_dict = {}
+#     for m_dir in multilingual_parallel_dirs:
+#         en_data = Path(f'{m_dir}/{lang}_en.txt').read_text().strip().split('\n')
+#         m_data = Path(f'{m_dir}/{lang}.txt').read_text().strip().split('\n')
+#         assert len(en_data) == len(m_data)
+#         source_name = m_dir.split('_')[0]
+#         for sent_en, sent_l in zip(en_data, m_data):
+#             if sent_en not in lang_source_dict:
+#                 lang_source_dict[sent_en] = {'source':[source_name],
+#                                           'trans': [sent_l]}
+#             else:
+#                 lang_source_dict[sent_en]['source'].append(source_name)
+#                 lang_source_dict[sent_en]['trans'].append(sent_l)
+#     return lang_source_dict
+#
+# def build_lang_pair(en_text_list, languages, multilingual_parallel_dirs, out_dir='cleaned_parallel'):
+#     out_dir = Path(out_dir)
+#     out_dir.mkdir(parents=True, exist_ok=True)
+#     for lang in tqdm(languages):
+#         meta = build_en_source_dict(lang, multilingual_parallel_dirs)
+#         out_path = Path(f'{out_dir}/en_{lang}.tsv')
+#         with out_path.open("w", encoding="utf-8", newline="") as f:
+#             writer = csv.writer(f, delimiter="\t")
+#             writer.writerow(["en", "trans", "source"])
+#
+#             for en_sent in en_text_list:
+#                 if en_sent not in meta:
+#                     continue
+#                 trans = meta[en_sent]["trans"][0]
+#                 source = "&".join(meta[en_sent]["source"])
+#                 writer.writerow([en_sent, trans, source])
 
 
 if __name__ =='__main__':
     parallel_dirs = sorted(glob('*_parallel/'))
     parallel_dirs = [x for x in parallel_dirs if 'merge' not in x]
-    langs = ['de', 'fr', 'zh', 'pl', 'ru', 'tr', 'ar','fi', 'ko']
+    langs = ['zh']
     en_list = clean_en(langs)
-    build_lang_pair(en_list, langs, parallel_dirs)
+    print(sum([len(x.split()) for x in en_list]))
+    print(clean_en(langs))
